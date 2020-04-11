8413c23863cda100a562c4e2cd7d54888c212754
William Berman <wlbberman@gmail.com>
2020-04-11 13:34:18 -0700

    Generic write directory

--- main.rkt
+++ main.rkt
@@ -7,7 +7,7 @@
 (require "optimizations.rkt")
 
 (define (test)
-  (install! 'fib (with-call-graph-save-and-display fib "~/home/wlbberman/call-graph.dot"))
+  (install! 'fib (with-call-graph-save-and-display fib "/tmp/call-graph.dot"))
   (fib 5)
   (define call-graph (gvector-ref call-graphs (- (gvector-count call-graphs) 1)))
   (define fib-5 (get-optimization call-graph fib))
